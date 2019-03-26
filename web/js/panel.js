6543407214b0253c4dc71041a8438c32aeceba35
hanss <hanssbcr@gmail.com>
2019-03-25 23:29:06 -0300

    localstorage tooltip published

--- web/js/panel.js
+++ web/js/panel.js
@@ -5,14 +5,53 @@ window.addEventListener("load", function () {
   console.log('pagina cargada!');
 });
 
-function toggleSidebar(){
+/* function toggleSidebar(){
     document.getElementById('sidebar').classList.toggle('active');
     document.getElementById('homea').classList.toggle('active');
     document.getElementById('eventosa').classList.toggle('active');
     document.getElementById('configa').classList.toggle('active');
     document.getElementById('salira').classList.toggle('active');
-}
+} */
 
+$('document').ready(function() {
+
+$('#collapse-toggle').on('click', function(e) {
+  localStorage.setItem('menu-closed', !$('#sidebar').hasClass('collapsed'));
+  $('#sidebar').toggleClass('collapsed');
+  $('#homea').toggleClass('desactive');
+  $('#eventosa').toggleClass('desactive');
+  $('#configa').toggleClass('desactive');
+  $('#salira').toggleClass('desactive');
+});
+ 
+// Code responsible for reading the state of the menu out of localStorage
+var state = localStorage.getItem('menu-closed');
+
+// When localStorage is not set, open the menu.
+if (state === null) {
+  
+  $('#sidebar').removeClass('collapsed');
+  $('#homea').removeClass('desactive');
+  $('#eventosa').removeClass('desactive');
+  $('#configa').removeClass('desactive');
+  $('#salira').removeClass('desactive');
+  
+} else {
+  
+  // When localStorage is set, Save the state to the variable closed
+  // Here set closed to a boolean true/false value instead of a string "true" or "false"      	
+  var closed = state === "true" ? true : false;
+  console.log(closed);
+  // When the state of the menu is not closed, remove the collapsed class from the sidebar.
+  if (!closed) {
+    $('#sidebar').removeClass('collapsed');
+    $('#homea').removeClass('desactive');
+  $('#eventosa').removeClass('desactive');
+  $('#configa').removeClass('desactive');
+  $('#salira').removeClass('desactive');
+  }
+}
+});
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -72,15 +111,22 @@ eventosRestClient.exportListEvent()
       console.log(arrayDt[i]);
       eventoscardsall.append('<div class="cardsevents"> <div class="titulocard" id="tituloeventsc'+i+'"><h4>'+arrayDt[i].name+'</h4></div> <div class="imgcontentcard"> <img class="imgcard" src="https://source.unsplash.com/random"></div> <div class="btncardbot"> <button id="bt_'+i+'" class="editbtn" ><i class="fas fa-pen"></i></button> <button class="editbtn" ><i class="fas fa-check"></i></button> <button class="editbtn" ><i class="fas fa-trash"></i></button>  <div></div> </div>')
       if(arrayDt[i].published){
-        $('#tituloeventsc'+i).append("<span class='spanevent'><i class='fas fa-check-circle'></i></span>");
+        $('#tituloeventsc'+i).append("<span class='spanevent subido'><i class='fas fa-check-circle'></i></span>");
       }else{
-        $('#tituloeventsc'+i).append("<span class='spanevent'><i class='fas fa-times'></i></span>");
+        $('#tituloeventsc'+i).append("<span class='spanevent nosubido'><i class='fas fa-times'></i></span>");
       }
       //$("#show_test").append("<div><a id='bt_"+i+"'>click"+arrayDt[i].name+"</a></div>");
       $("#bt_"+i).click(function(){
          show_data(arrayDt[i]);
       });
+      
   });
+  tippy('.subido', {
+    content: "Este evento esta publicado!",
+  })
+  tippy('.nosubido', {
+    content: "Este evento aun NO esta publicado!",
+  })
     /* events.forEach(function(event){
       var titulo = event.title;
       var ids = event.id;
