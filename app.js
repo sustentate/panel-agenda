2f8bcc5907fa5ec73b31ea7e85296565be20a97a
hanss11 <hanssbcr@gmail.com>
2019-04-06 15:27:15 -0300

    prueba de cors

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next) {
+  res.send(/web/eventos.html);
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
